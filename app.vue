7921c33efed05b460ee34c9c2486d0b014f09380
Konrad Gladysz <16876426+konrad-multibyte@users.noreply.github.com>
2023-07-16 23:39:22 +0100

    feat(navbar): basic navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Navbar />
+  <NuxtPage />
 </template>
+
+<script>
+
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar">
+        <div class="nav-item">
+            <a class="nav-link" href="">Processes</a>
+        </div>
+        <div class="nav-item">
+            <a class="nav-link" href="">Forms</a>
+        </div>
+        <div class="nav-item">
+            <a class="nav-link" href="">Apps</a>
+        </div>
+    </nav>
+</template>
