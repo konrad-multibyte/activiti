103b3d61412ae9d946d3173b084920e95b8945cc
Konrad Gladysz <16876426+konrad-multibyte@users.noreply.github.com>
2023-07-16 19:22:27 +0100

    feat(test): tests are now working

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    plugins: [vue()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+    },
+    root: ".",
+})
