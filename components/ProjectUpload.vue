7fab136c9dd211650ffcd311fd1c76380c3c6dae
Konrad Gladysz <16876426+konrad-multibyte@users.noreply.github.com>
2023-07-17 02:24:53 +0100

    fix(upload-project): linting

--- components/ProjectUpload.vue
+++ components/ProjectUpload.vue
@@ -2,12 +2,7 @@
     <form @submit.prevent="sendFile" action="" enctype="multipart/form-data">
         <div class="field">
             <label for="project">Upload Project</label>
-            <input
-                type="file"
-                name="file"
-                ref="file"
-                id="project" 
-                @change="selectFile">
+            <input type="file" name="file" ref="file" id="project" @change="selectFile">
         </div>
         <div class="field">
             <button class="button button-primar">Upload</button>
@@ -16,31 +11,31 @@
 </template>
 
 <script>
-    export default {
-        name: "ProjectUpload",
+export default {
+    name: "ProjectUpload",
 
-        data() {
-            return {
-                file: ""
-            };
+    data() {
+        return {
+            file: ""
+        };
+    },
+
+    methods: {
+        selectFile() {
+            this.file = this.$refs.file.files[0];
         },
-
-        methods: {
-            selectFile() {
-                this.file = this.$refs.file.files[0];
-            },
-            async sendFile() {
-                const formData = new FormData();
-                formData.append("file", this.file);
-                try {
-                    await $fetch("/api/project", {
-                        method: "post",
-                        body: formData
-                    });
-                } catch(error) {
-                    console.log(error);
-                }
+        async sendFile() {
+            const formData = new FormData();
+            formData.append("file", this.file);
+            try {
+                await $fetch("/api/project", {
+                    method: "post",
+                    body: formData
+                });
+            } catch (error) {
+                console.log(error);
             }
         }
     }
+}
 </script>
