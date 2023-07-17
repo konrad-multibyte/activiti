54fb22c1c61d4630360c411dd832ac912a2a31c6
Konrad Gladysz <16876426+konrad-multibyte@users.noreply.github.com>
2023-07-17 18:33:53 +0100

    feat(upload-project): project upload to bucket

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     css: [`assets/css/main.css`],
     runtimeConfig: {
-        mongodb: process.env.MONGODB
+        mongoUri: process.env.MONGO_URI,
+        mongoDb: process.env.MONGO_DB,
+        s3Uri: process.env.S3_URI,
+        s3Region: process.env.S3_REGION,
+        s3Key: process.env.S3_KEY,
+        s3AccessKey: process.env.S3_ACCESSKEY,
+        s3Bucket: process.env.S3_BUCKET
     }
 })
--- server/api/project.post.ts
+++ server/api/project.post.ts
@@ -1,31 +1,65 @@
 import { writeFile } from "fs"
 import { join } from "path";
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client, CompleteMultipartUploadCommandOutput } from "@aws-sdk/client-s3";
 
 export default defineEventHandler(async (event) => {
     const files = await readMultipartFormData(event);
     const config = useRuntimeConfig(event);
-    const client = new MongoClient(config.mongodb, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
     if (files !== undefined) {
-        for (let file of files) {
-            const metadata = {
-                filename: file.filename,
-                name: file.name,
-                type: file.type
+        const client = new MongoClient(config.mongoUri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
             }
-            try {
-                await client.connect();
-                await client.db("activiti").collection("projects").insertOne(metadata);
-            } finally {
-                await client.close();
+        });
+        try {
+            await client.connect();
+            for (let file of files) {
+                const metadata = {
+                    filename: file.filename,
+                    name: file.name,
+                    type: file.type
+                };
+                try {
+                    const response = await new Upload({
+                        client: new S3Client({
+                            endpoint: config.s3Uri,
+                            credentials: {
+                                accessKeyId: config.s3Key,
+                                secretAccessKey: config.s3AccessKey
+                            },
+                            region: config.s3Region
+                        }),
+                        params: {
+                            Bucket: config.s3Bucket,
+                            Key: file.filename,
+                            Body: file.data
+                        }
+                    }).done();
+                    if (response instanceof Object && "ETag" in response) {
+                        const metadata = {
+                            filename: file.filename,
+                            name: file.name,
+                            type: file.type,
+                            etag: response.ETag,
+                            bucket: response.Bucket,
+                            location: response.Location
+                        };
+                        await client.db(config.mongoDb).collection("projects").insertOne(metadata);
+                    } else {
+                        console.log("Upload failed." + response)
+                    }
+
+                } catch (error) {
+                    console.log(error);
+                }
+                writeFile(join(".", "uploads", "file"), file.data, () => { console.log("Written to disk.") });
             }
-            writeFile(join(".", "uploads", "file"), file.data, () => { console.log("Written to disk.") });
+        } finally {
+            await client.close();
         }
     }
     return { files };
