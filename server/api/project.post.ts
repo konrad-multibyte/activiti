4583289410e602aad66def4a6a0c16fe4d13afed
Konrad Gladysz <16876426+konrad-multibyte@users.noreply.github.com>
2023-07-17 14:21:38 +0100

    feat(upload-project): file metadata now uploaded

--- server/api/project.post.ts
+++ server/api/project.post.ts
@@ -1,11 +1,30 @@
 import { writeFile } from "fs"
 import { join } from "path";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 export default defineEventHandler(async (event) => {
     const files = await readMultipartFormData(event);
-    console.log(files)
+    const config = useRuntimeConfig(event);
+    const client = new MongoClient(config.mongodb, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
     if (files !== undefined) {
         for (let file of files) {
+            const metadata = {
+                filename: file.filename,
+                name: file.name,
+                type: file.type
+            }
+            try {
+                await client.connect();
+                await client.db("activiti").collection("projects").insertOne(metadata);
+            } finally {
+                await client.close();
+            }
             writeFile(join(".", "uploads", "file"), file.data, () => { console.log("Written to disk.") });
         }
     }
