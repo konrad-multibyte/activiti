051006a8d99983d678b693b427eb9ff6a4b3ba33
Konrad <16876426+konrad-multibyte@users.noreply.github.com>
2023-07-19 11:50:50 +0100

    feat(upload-project): get proejct details

--- server/api/project/[id].get.ts
+++ server/api/project/[id].get.ts
@@ -1,4 +1,22 @@
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+
 export default defineEventHandler(async (event) => {
-    const { filename } = event.context.params;
-    console.log(filename);
+    // @ts-ignore
+    const { id } = event.context.params;
+    const config = useRuntimeConfig(event);
+    const client = new MongoClient(config.mongoUri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    try {
+        await client.connect();
+        const project = await client.db(config.mongoDb).collection("projects").findOne( {_id: new ObjectId(id) });
+        return { project };
+    }
+    finally {
+        await client.close();
+    }
 });
